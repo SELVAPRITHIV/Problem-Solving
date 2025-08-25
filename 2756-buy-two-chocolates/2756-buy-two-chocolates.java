442fbc8b3c851bf90bdb65deba95fe8bbd1f0d1b
SELVAPRITHIV <118437390+SELVAPRITHIV@users.noreply.github.com>
2025-08-25 11:40:52 +0530

    Time: 2 ms (66.64%), Space: 44.3 MB (84.34%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -3,7 +3,7 @@ class Solution {
         int n = prices.length;
         Arrays.sort(prices);
         int min = 0;
-        for(int i=0;i<3-2;i++){
+        for(int i=0;i<2-1;i++){
            if((prices[i]+prices[i+1])<=money){
             min = money - (prices[i]+prices[i+1]);
             return min;
